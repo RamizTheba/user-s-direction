8c5ff7dfa8e29242dc19b7a82e87ef3950239471
dev_ramiz_1707 <ramiz.gameti534@gmail.com>
2019-12-07 00:34:41 +0800

    direction based on role

--- admin/setting-page.php
+++ admin/setting-page.php
@@ -1,7 +1,10 @@
 
 <?php
 
-function get_r() {
+global $wpdb;
+$UD_table = $wpdb->prefix . 'ud_role_directions';
+
+function UD_get_roles() {
   global $wp_roles;
 
   $all_roles = $wp_roles->roles;
@@ -10,7 +13,35 @@ function get_r() {
   return $all_roles;
 }
 
-$pages = get_pages();
+
+function UD_get_select_page_dropdown($role_key, $selected_role_key){
+  $is_selected = "";
+  $pages = get_pages();
+
+  ?>
+
+    <select class="form-control <?php echo $role_key; ?>" name="some_input_counter">
+
+      <?php 
+
+      foreach( $pages as $key_page => $value_page ){
+
+        if($selected_role_key == $value_page->ID){
+          $is_selected = "selected";
+        }else{
+          $is_selected = "";
+        }
+        echo "<option value='".$value_page->ID."' ".$is_selected.">".$value_page->post_title."</option>";
+        //echo " key page ". $value_page->ID ." => name ".$value_page->post_title."<br>";
+      }
+
+      ?>
+
+    </select> 
+
+  <?php
+}
+
 
 //echo "<pre>";
 //print_r($pages);
@@ -25,7 +56,7 @@ $pages = get_pages();
 <div class="UD_container container">
 
   <div class="UD_plugin_head">
-    <h3>Welcome To User's direction </h3>
+    <h3>Welcome To User's direction <span class="UD-direction-icon"><img class="rounded" src="<?php echo plugins_url('/users-directions/assets/img/direction.png'); ?>" /></span></h3>
     <p class="UD_plugin_quote">Easy to give direction to users on the basis of their role</p>
   </div>
   
@@ -53,47 +84,71 @@ $pages = get_pages();
             <form id="form">
               
               <div class="heading_table">
-                <div>Select the role of user and the appropriate pages</div>              
+                <div>Select the role of user and the appropriate pages ( <input type="checkbox" class="form-control" id="test" checked="checked"></input> tik to active redirection of particular role )</div>              
               </div>
 
               <div class="parent_container">
                 
                 <div class="template_element">
-                  <!-- <label for="some_input_counter">Input counter</label> -->
-                  <select class="form-control" name="some_input_counter">
-                    <?php
-                      foreach(get_r() as $key => $value){
+
+                <?php
+                      //$i_inc = 1;
+                      foreach(UD_get_roles() as $UD_role_key => $UD_role_key){
+                          $UD_find_in_db = $wpdb->get_results("SELECT * FROM $UD_table where role = '".$UD_role_key."'");
+                          //echo "<pre>";
+                          //print_r($UD_find_in_db[0]->redirect_url);
+                          //echo "</pre>";
+                        ?>
+
+                          <div class="form-row">
+                              <div class="col mt-2">
+                                <input type="text" class="form-control" value="<?php echo $UD_role_key; ?>" placeholder="<?php echo $UD_role_key['name']; ?>" readonly>
+                              </div>
+                              <div class="col mt-2">
+                                <!-- <input type="text" class="form-control" placeholder="Last name"> -->
+                                <?php 
+                                    // echo "<pre>";
+                                    // print_r($UD_find_in_db);
+                                    // echo "<pre>";
+                                    // exit();
+                                    $redirect_urls = home_url();
+                                    $redirect_checked = false;
+                                    if( !empty( $UD_find_in_db ) ){
+                                      $redirect_urls = $UD_find_in_db[0]->redirect_url;
+                                      if( $UD_find_in_db[0]->redirect_url != "" )
+                                      $redirect_checked = true;
+                                    } 
+                                    
+                                    UD_get_select_page_dropdown($UD_role_key, $redirect_urls);
+                                ?>
+                              </div>
+                              <div class="col mt-2">
+                                <input type="checkbox" class="form-control direction_section direction_section_<?php echo $UD_role_key;  ?> <?php if( $redirect_checked ){ echo "checked";  }  ?>" id="<?php echo $UD_role_key;  ?>" <?php if( $redirect_checked ){ echo "checked='checked'";  }  ?>>                        
+                                <!-- <button type="button" class="remove btn btn-danger">x</button> -->
+                              </div>
+                            </div>
+
+
+                        <?php
+
                         //echo "Role : ". $key ." => name  ".$value['name']."<br>"; 
-                        echo "<option value='".$key."'>".$value['name']."</option>";
+                        //echo "<option value='".$key."'>".$value['name']."</option>";
+
+                        //$i_inc++;
                       }
                       
-                    ?>
-                    <!-- <option>Administrator</option>                  
-                    <option>Administrator</option>                  
-                    <option>Administrator</option>                   -->
-
-                  </select>
-                  <select class="form-control" name="some_input_counter">
-                    <?php 
-
-                    foreach( $pages as $key_page => $value_page ){
-                      echo "<option value='".$value_page->ID."'>".$value_page->post_title."</option>";
-                      //echo " key page ". $value_page->ID ." => name ".$value_page->post_title."<br>";
-                    }
-
+              
                     ?>
                                 
-                  </select>
-                  <!-- <a href="#" class="remove button btn btn-danger">x</a> -->
-                  <button type="button" class="remove btn btn-danger">x</button>
+              
                 </div>
                 
               </div>
               
-              <a href="#" class="add_field btn_add_field button button-primary">Add Field</a>
 
               <div class="UD-btn-save mt-3">
                 <button onclick="return false;" class="btn-save btn btn-success" >Save</button>
+                <div class="update_message"></div>
               </div>
             </form>
         </div>
@@ -110,16 +165,6 @@ $pages = get_pages();
           
           <div class="heading_table">
             <div>Please select or check the option you want to setup</div>              
-          </div>
-
-          <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="defaultUnchecked">
-            <label class="custom-control-label" for="defaultUnchecked">Do you want to delete plugin data after the plugin has deactivated</label>
-          </div>
-
-          <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="defaultUnchecked">
-            <label class="custom-control-label" for="defaultUnchecked">Do you want to delete plugin data after the plugin has deactivated</label>
           </div>
 
           <div class="custom-control custom-checkbox">
